e97c40f8c0d4256486e3e37bd1171dc4342e9cb5
Rodrigo M <izeka1983@hotmail.com>
2018-04-09 10:26:56 -0300

    update settings

--- mosto/settings.py
+++ mosto/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'fullurl',
     'extra_views',
     'qrcode',
     'widget_tweaks',
